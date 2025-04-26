7ec2ddaba09e96bacccb3f06e031ed4ac152d04b
Ankit Parwatkar <ankitparwatkar35@gmail.com>
2025-04-26 15:05:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ st.markdown("""
         color: white;
     }
     .feature-importance {
-        background-color: #AFEEEE;
+        background-color: #D2B48C;
         padding: 20px;
         border-radius: 10px;
         box-shadow: 0 4px 6px rgba(0,0,0,0.1);
